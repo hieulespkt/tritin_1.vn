0227de9a7816130e8990f6780ee9f7ef667d5592
Le Nhat Hieu <hieulespkt94@gmail.com>
2015-09-13 07:25:32 +0700

    tạo master_page, add css,js,image

---
+++ resources/views/share/__layout.blade.php
@@ -0,0 +1,360 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>Tools Shop</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <link rel="stylesheet" type="text/css" href="css/font-awesome.css" />
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/boxOver.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function(){
+            $(".button-show-bar i").click(function(){
+                $("ul.menu").slideToggle("slow");
+            });
+        });
+    </script>
+</head>
+<body>
+<div class="pull-container width-100">
+<div id="main_container">
+<div id="header">
+    <img src="images/tritin.vn/logo.png">
+</div>
+<div id="main_content">
+<div id="menu_tab">
+    <div class="button-show-bar">
+        <i class="fa fa-bars"></i>
+    </div>
+    <ul class="menu">
+        <li><a href="#" class="nav"> Trang chủ</a></li>
+
+        <li><a href="#" class="nav">Giới thiệu</a>
+            <ul>
+                <li><a href="#">item 1</a></li>
+                <li><a href="#">item 1</a></li>
+                <li><a href="#">item 1</a></li>
+            </ul>
+        </li>
+
+        <li>
+            <a href="#" class="nav">Sản phẩm</a>
+            <ul class="multi-menu min-width-ul">
+                <li>
+                    <div class="main-con-menu row mr-none">
+                        <div class="item-cont-menu col-md-3">
+                            <p>Tên sản phẩm</p>
+                            <ul>
+                                <li><a href="#"><i class="fa fa-angle-right"></i>item 1</a></li>
+                                <li><a href="#"><i class="fa fa-angle-right"></i>item 2</a></li>
+                                <li><a href="#"><i class="fa fa-angle-right"></i>item 3</a></li>
+                            </ul>
+                        </div>
+                        <div class="item-cont-menu col-md-3">
+                            <p>Tên sản phẩm</p>
+                            <ul>
+                                <li><a href="#"><i class="fa fa-angle-right"></i>item 1</a></li>
+                                <li><a href="#"><i class="fa fa-angle-right"></i>item 2</a></li>
+                                <li><a href="#"><i class="fa fa-angle-right"></i>item 3</a></li>
+                            </ul>
+                        </div>
+                        <div class="item-cont-menu col-md-3">
+                            <p>Tên sản phẩm</p>
+                            <ul>
+                                <li><a href="#"><i class="fa fa-angle-right"></i>item 1</a></li>
+                                <li><a href="#"><i class="fa fa-angle-right"></i>item 2</a></li>
+                                <li><a href="#"><i class="fa fa-angle-right"></i>item 3</a></li>
+                            </ul>
+                        </div>
+                        <div class="item-cont-menu col-md-3">
+                            <p>Tên sản phẩm</p>
+                            <ul>
+                                <li><a href="#"><i class="fa fa-angle-right"></i>item 1</a></li>
+                                <li><a href="#"><i class="fa fa-angle-right"></i>item 2</a></li>
+                                <li><a href="#"><i class="fa fa-angle-right"></i>item 3</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </li>
+
+        <li><a href="#" class="nav">Dịch vụ</a></li>
+
+        <li><a href="#" class="nav">Giải pháp</a></li>
+
+        <li><a href="#" class="nav">Tin tức</a></li>
+
+        <li><a href="contact.html" class="nav">Hổ trợ khách hàng</a></li>
+
+        <li><a href="details.html" class="nav">Liên hệ</a></li>
+    </ul>
+</div>
+<div class="main-content row">
+<!-- end of menu tab -->
+<div class="left-content-re col-md-2">
+    <p>Danh mục sản phẩm</p>
+    <div class="con-left">
+        <div class="title-name">
+            <p>SKF</p>
+        </div>
+        <ul>
+            <li><a href="#">Ổ lăn và cụm ổ lăn</a></li>
+            <li><a href="#">Phớt</a></li>
+            <li><a href="#">Sản phẩm truyền động</a></li>
+            <li><a href="#">D.cụ bảo trì, bôi trơn</a></li>
+        </ul>
+    </div>
+    <div class="con-left">
+        <div class="title-name">
+            <p>HABASIT</p>
+        </div>
+        <ul>
+            <li><a href="#">Plastic Modular Belt</a></li>
+            <li><a href="#">Slat Top Chain</a></li>
+            <li><a href="#">CleanDrive</a></li>
+        </ul>
+    </div>
+    <div class="con-left">
+        <div class="title-name">
+            <p>ASHWORTH</p>
+        </div>
+        <ul>
+            <li><a href="#">Băng tải inox Cleatrac</a></li>
+            <li><a href="#">Băng tải inox Eye-link</a></li>
+        </ul>
+    </div>
+    <div class="con-left">
+        <div class="title-name">
+            <p>ALFA LAVAL</p>
+        </div>
+        <ul>
+            <li><a href="#">Dàn lạnh tấm (PHE)</a></li>
+            <li><a href="#">Băng tải inox Eye-link</a></li>
+        </ul>
+    </div>
+    <div class="con-left">
+        <div class="title-name">
+            <p>Sản phẩm mới</p>
+        </div>
+        <div class="border_box">
+            <div class="product_title"><a href="#">Makita 156 MX-VL</a></div>
+            <div class="product_img"><a href="#"><img src="images/product/p1.jpg" alt="" border="0" /></a></div>
+            <div class="prod_price"><span class="reduce">350$</span> <span class="price">270$</span></div>
+        </div>
+    </div>
+</div>
+<!-- end of left content -->
+<div class="center-content-re col-md-8">
+    <div class="center_content ">
+        <div class="oferta"> <img src="images/product/p1.png" width="165" height="113" border="0" class="oferta_img" alt="" />
+            <div class="oferta_details">
+                <div class="oferta_title">Power Tools BST18XN Cordless</div>
+                <div class="oferta_text"> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco </div>
+                <a href="#" class="prod_buy">details</a> </div>
+        </div>
+        <div class="center_title_bar">Sản phẩm mới</div>
+        <div class ="show-product row">
+            <div class="prod_box pdright-10-im pdbottom-10-im  col-md-4 col-sm-4 col-xs-12">
+                <div class="center_prod_box">
+                    <div class="cont-product">
+                        <div class="product_title">Makita 156 MX-VL</div>
+                        <div class="product_img"><img src="images/product/p7.jpg" alt="" border="0" /></div>
+                        <div class="prod_price"><span class="reduce">350$</span> <span class="price">270$</span>
+                        </div>
+                    </div>
+                    <div class="center-prod-ct">
+                        <a href="#">
+                            <div class="cen-ct-con">
+                                <ul>
+                                    <li>Chi tiet 1</li>
+                                    <li>Chi tiet 1</li>
+                                    <li>Chi tiet 1</li>
+                                </ul>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+            </div>
+            <div class="prod_box pdright-10-im pdbottom-10-im  col-md-4 col-sm-4 col-xs-12">
+                <div class="center_prod_box">
+                    <div class="cont-product">
+                        <div class="product_title">Makita 156 MX-VL</div>
+                        <div class="product_img"><img src="images/product/p7.jpg" alt="" border="0" /></div>
+                        <div class="prod_price"><span class="reduce">350$</span> <span class="price">270$</span>
+                        </div>
+                    </div>
+                    <div class="center-prod-ct">
+                        <a href="#">
+                            <div class="cen-ct-con">
+                                <ul>
+                                    <li>Chi tiet 1</li>
+                                    <li>Chi tiet 1</li>
+                                    <li>Chi tiet 1</li>
+                                </ul>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+            </div>
+            <div class="prod_box pdright-10-im pdbottom-10-im  col-md-4 col-sm-4 col-xs-12">
+                <div class="center_prod_box">
+                    <div class="cont-product">
+                        <div class="product_title">Makita 156 MX-VL</div>
+                        <div class="product_img"><img src="images/product/p7.jpg" alt="" border="0" /></div>
+                        <div class="prod_price"><span class="reduce">350$</span> <span class="price">270$</span>
+                        </div>
+                    </div>
+                    <div class="center-prod-ct">
+                        <a href="#">
+                            <div class="cen-ct-con">
+                                <ul>
+                                    <li>Chi tiet 1</li>
+                                    <li>Chi tiet 1</li>
+                                    <li>Chi tiet 1</li>
+                                </ul>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+            </div>
+            <div class="prod_box pdright-10-im pdbottom-10-im  col-md-4 col-sm-4 col-xs-12">
+                <div class="center_prod_box">
+                    <div class="cont-product">
+                        <div class="product_title">Makita 156 MX-VL</div>
+                        <div class="product_img"><img src="images/product/p7.jpg" alt="" border="0" /></div>
+                        <div class="prod_price"><span class="reduce">350$</span> <span class="price">270$</span>
+                        </div>
+                    </div>
+                    <div class="center-prod-ct">
+                        <a href="#">
+                            <div class="cen-ct-con">
+                                <ul>
+                                    <li>Chi tiet 1</li>
+                                    <li>Chi tiet 1</li>
+                                    <li>Chi tiet 1</li>
+                                </ul>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+            </div>
+            <div class="prod_box pdright-10-im pdbottom-10-im  col-md-4 col-sm-4 col-xs-12">
+                <div class="center_prod_box">
+                    <div class="cont-product">
+                        <div class="product_title">Makita 156 MX-VL</div>
+                        <div class="product_img"><img src="images/product/p7.jpg" alt="" border="0" /></div>
+                        <div class="prod_price"><span class="reduce">350$</span> <span class="price">270$</span>
+                        </div>
+                    </div>
+                    <div class="center-prod-ct">
+                        <a href="#">
+                            <div class="cen-ct-con">
+                                <ul>
+                                    <li>Chi tiet 1</li>
+                                    <li>Chi tiet 1</li>
+                                    <li>Chi tiet 1</li>
+                                </ul>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <div class="center_title_bar">Recomended Products</div>
+        <div class ="show-product row">
+            <div class="prod_box pdright-10-im pdbottom-10-im  col-md-4 col-sm-4 col-xs-12">
+                <div class="center_prod_box">
+                    <div class="product_title"><a href="#">Makita 156 MX-VL</a></div>
+                    <div class="product_img"><a href="#"><img src="images/product/p7.jpg" alt="" border="0" /></a></div>
+                    <div class="prod_price"><span class="reduce">350$</span> <span class="price">270$</span></div>
+                </div>
+            </div>
+            <div class="prod_box pdright-10-im pdbottom-10-im  col-md-4 col-sm-4 col-xs-12">
+                <div class="center_prod_box">
+                    <div class="product_title"><a href="#">Bosch XC</a></div>
+                    <div class="product_img"><a href="#"><img src="images/product/p1.jpg" alt="" border="0" /></a></div>
+                    <div class="prod_price"><span class="reduce">350$</span> <span class="price">270$</span></div>
+                </div>
+            </div>
+            <div class="prod_box col-md-4 col-sm-4 col-xs-12">
+                <div class="center_prod_box">
+                    <div class="product_title"><a href="#">Lotus PP4</a></div>
+                    <div class="product_img"><a href="#"><img src="images/product/p3.jpg" alt="" border="0" /></a></div>
+                    <div class="prod_price"><span class="reduce">350$</span> <span class="price">270$</span></div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
+<!-- end of center content -->
+<div class="right-content-re col-md-2">
+    <div class="con-left">
+        <div class="border_box">
+            <div class="input-group input-group-sm pdtop-10">
+                <span class="input-group-addon" id="sizing-addon3"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></span>
+                <input type="text" class="form-control" placeholder="Tìm kiếm" aria-describedby="sizing-addon3">
+            </div>
+        </div>
+    </div>
+
+    <div class="con-left">
+        <div class="title-name">
+            <p>Có gì mới?</p>
+        </div>
+        <div class="border_box">
+            <div class="product_title"><a href="#">Motorola 156 MX-VL</a></div>
+            <div class="product_img"><a href="#"><img src="images/product/p2.jpg" alt="" border="0" /></a></div>
+            <div class="prod_price"><span class="reduce">350$</span> <span class="price">270$</span></div>
+        </div>
+    </div>
+    <div class="con-left">
+        <div class="title-name">
+            <p>Tên danh mục</p>
+        </div>
+        <ul>
+            <li><a href="#">Air Tools</a></li>
+            <li><a href="#">Hand Tools</a></li>
+            <li><a href="#">Accessories</a></li>
+            <li><a href="#">Workwear</a></li>
+            <li><a href="#">Spare Parts</a></li>
+            <li><a href="#">Power Tools</a></li>
+            <li><a href="#">Air Tools</a></li>
+            <li><a href="#">Hand Tools</a></li>
+            <li><a href="#">Accessories</a></li>
+            <li><a href="#">Workwear</a></li>
+            <li><a href="#">Spare Parts</a></li>
+        </ul>
+    </div>
+    <div class="banner_adds"> <a href="#"><img src="images/product/bann1.jpg" alt="" border="0" /></a> </div>
+</div>
+</div>
+<!-- end of right content -->
+</div>
+</div>
+<!-- end of main content -->
+<div class="footer">
+    <div class="left_footer"> <img src="images/tritin.vn/footer_logo.png" alt="" width="89" height="42"/> </div>
+    <div class="center_footer"> Template name. All Rights Reserved 2008<br />
+        <a href="http://csscreme.com"><img src="images/product/csscreme.jpg" alt="csscreme" title="csscreme" border="0" /></a><br />
+        <img src="images/product/payment.gif" alt="" /> </div>
+    <div class="right_footer"> <a href="#">home</a> <a href="#">about</a> <a href="#">sitemap</a> <a href="#">rss</a> <a href="#">contact us</a> </div>
+</div>
+</div>
+<!-- end of main_container -->
+<div align=center>This template  downloaded form <a href='#'>free website templates</a></div>
+</div>
+</body>
+</html>
